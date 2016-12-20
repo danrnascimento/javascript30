587254e375975674178453b3e2aa6bb0fd71ab1b
danrnascimento <danrnascimento@gmail.com>
2016-12-19 23:35:29 -0200

    Nyan Cat Drum Kit

--- Day1 - JavaScript Drum Kit/js/drumkit.js
+++ Day1 - JavaScript Drum Kit/js/drumkit.js
@@ -1,13 +1,11 @@
 function onPlay(e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //select the audio tag by key code
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //select the div tag by key code
-	
 	if(!audio) return; //stop the function from running all together
-
+	
+	key.classList.add('playing');
 	audio.currentTime = 0;//rewind to the start
 	audio.play(); //play audio
-
-	key.classList.add('playing');
 }
 
 function removeTransition(e) {
@@ -17,5 +15,4 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
 window.addEventListener('keydown', onPlay);

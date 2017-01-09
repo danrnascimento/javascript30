850431a20605d115d3dbec6d67975be6cf736235
danrnascimento <danrnascimento@gmail.com>
2017-01-08 22:07:06 -0200

    Day 6

--- Day6 - Ajax Type Ahead/js/ajax.type-ahead.js
+++ Day6 - Ajax Type Ahead/js/ajax.type-ahead.js
@@ -3,11 +3,11 @@ const cities = [];
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
-fetch(endpoint)
+fetch(endpoint) //get data
 	.then(blob => blob.json())
 	.then(data => cities.push(...data));
 
-function findMacthes(wordToMatch, cities){
+function findMacthes(wordToMatch, cities){ //filter the cities in array
 
 	return cities.filter(place => {
 		const regex = new RegExp(wordToMatch, 'gi');
@@ -15,34 +15,35 @@ function findMacthes(wordToMatch, cities){
 	});
 }
 
-function numberWithCommas(x) {
+function numberWithCommas(x) { //use comma in the numbers
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 function displayMatches() {
 	const matchArray = findMacthes(this.value, cities);
 	const html = matchArray.map(place => {
-		const regex = new RegExp(this.value, 'gi');
-		const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-		const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
+		const regex = new RegExp(this.value, 'gi'); //create new regex to highlight the word
+		const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);  //apply highlight in city name
+		const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`); //apply highlight in state name
 
+		//return this html for each city
 		return `
 			<li data-lat="${place.latitude}" data-lng="${place.longitude}">
 			<span class="name"> ${cityName}, ${stateName}</span>
 			<span class="population">${numberWithCommas(place.population)}</span>
 			</li>
 		`;
+
 	}).join('');
-	suggestions.innerHTML = html;
+	suggestions.innerHTML = html; //include the li in the page
 
-	const locals = document.querySelectorAll('li');
-	function updateMarker(){
-		console.log(map);
-		map.setCenter({ lat: parseFloat(this.dataset.lat), lng: parseFloat(this.dataset.lng) });
+	const locals = document.querySelectorAll('li'); //get all Li with the locations
+	function updateMarker(){ //function for update the map
+		map.setCenter({ lat: parseFloat(this.dataset.lat), lng: parseFloat(this.dataset.lng) }); //update the map
 	}
-	locals.forEach(local => local.addEventListener('click', updateMarker));
+	locals.forEach(local => local.addEventListener('click', updateMarker)); //event to execute the function for update the map
 }
-
+//init google maps API
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: -34.397, lng: 150.644},
@@ -52,9 +53,4 @@ function initMap() {
 }
 
 
-
 searchInput.addEventListener('keyup', displayMatches);
-
-
-
-

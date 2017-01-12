fba9081da4fec446956aa4441a324fde66f94318
daniel <drnacimento@stefanini.com>
2017-01-12 18:08:49 -0200

    Dia 8

--- Day8 - Fun with HTML5 Canvas/js/canvas.js
+++ Day8 - Fun with HTML5 Canvas/js/canvas.js
@@ -1,22 +1,57 @@
-
-const canvas = document.querySelector('.draw');
+const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 
-ctx.strokeStyle = '#BADA55';
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 1;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
+
 
 function draw (e) {
+	
 	if(!isDrawing) return false;
-	console.log(e);
+
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+	ctx.beginPath();
+
+	ctx.moveTo(lastX, lastY);
+
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+	[lastX, lastY] = [e.offsetX, e.offsetY];
+
+
+	hue++;
+	if(hue >= 360) {
+		hue = 0;
+	}
+
+	if(ctx.lineWidth >= 120 || ctx.lineWidth <= 1){
+		direction = !direction;
+	}
+
+	if(direction){
+		ctx.lineWidth++;
+	}else{
+		ctx.lineWidth--;
+	}
 }
 
 
-canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mousedown', (e) => {
+	isDrawing = true;
+	[lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
-canvas.addEventListener('mousemove', draw);

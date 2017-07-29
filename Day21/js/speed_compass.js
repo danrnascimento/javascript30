d12d0466316382b00e04d736b66b9cbd86225047
Daniel Nascimento <danrnascimento@gmail.com>
2017-07-29 15:11:54 -0300

    Day 21

---
+++ Day21/js/speed_compass.js
@@ -0,0 +1,17 @@
+const speed = document.querySelector('.speed');
+const compass = document.querySelector('svg');
+
+const compassFunction = function (data) {
+    speed.textContent = data.coords.speed;
+    compass.style.transform = `rotate(${data.coords.heading}deg)`;
+
+    if(data.coords.speed == null || data.coords.heading == null) {
+        alert('Please, use a device with speedometer and compass');
+    }
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((data) => compassFunction(data), (err) => {
+        console.log(err);
+    });
+}
